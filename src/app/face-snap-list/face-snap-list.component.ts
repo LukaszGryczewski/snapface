f8934617fade4bf311a50b56a24104329eacfbdc
Lukasz <luka.gryczewski@gmail.com>
2024-04-21 20:36:24 +0200

    Create a FaceSnap service

--- src/app/face-snap-list/face-snap-list.component.ts
+++ src/app/face-snap-list/face-snap-list.component.ts
@@ -1,8 +1,8 @@
+import { FaceSnapService } from './../services/face-snap.service';
 import { Component,Input, OnInit } from '@angular/core';
 import { FaceSnap } from '../models/face-snap.models';
 import { CommonModule } from '@angular/common';
 import { FaceSnapComponent } from '../face-snap/face-snap.component';
-//import { FaceSnapComponent } from './face-snap/face-snap.component';
 
 
 @Component({
@@ -15,31 +15,9 @@ import { FaceSnapComponent } from '../face-snap/face-snap.component';
 export class FaceSnapListComponent implements OnInit{
   faceSnaps! : FaceSnap[];
 
-  ngOnInit(){
-    this.faceSnaps = [
-      {
-        title : 'Archibald',
-        description : 'Mon meilleur ami depuis tout petit !',
-        imageUrl : 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
-        createdDate: new Date(),
-        location : "Anvers",
-        snaps : 50
+  constructor(private FaceSnapService : FaceSnapService){}
 
-      },
-      {
-        title: 'Three Rock Mountain',
-        description: 'Un endroit magnifique pour les randonnées.',
-        imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Three_Rock_Mountain_Southern_Tor.jpg/2880px-Three_Rock_Mountain_Southern_Tor.jpg',
-        createdDate: new Date(),
-        snaps: 325
-      },
-      {
-        title: 'Un bon repas',
-        description: 'Mmmh que c\'est bon !',
-        imageUrl: 'https://wtop.com/wp-content/uploads/2020/06/HEALTHYFRESH.jpg',
-        createdDate: new Date(),
-        snaps: 5
-      }
-    ];
+  ngOnInit(){
+    this.faceSnaps = this.FaceSnapService.faceSnaps;
   }
 }
--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -12,21 +12,10 @@ import { CommonModule } from '@angular/common';
 export class FaceSnapComponent {
   @Input() faceSnap! : FaceSnap;
 
-  /*title!       : string;
-  description! : string;
-  createDate!  : Date;
-  snap!        : number;
-  imageUrl!    : string;*/
   buttonText!  : string;
 
   ngOnInit(): void {
-    /*this.title       = "Lukasz";
-    this.description = "Developer / Analyste";
-    this.createDate  = new Date();
-    this.snap        = 6;
-    this.imageUrl    = '/assets/images/Jizo.jpg';*/
     this.buttonText  = "Oh Snap!";
-
   }
 
   onSnap(){
